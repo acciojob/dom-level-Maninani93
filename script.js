c59d7d9637e852a8902d7c3d650c8d3c4dc44850
Maninani93 <javvadinani422@gmail.com>
2025-03-03 02:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+let element = document.getElementById("level");
+let level = 0;
+
+// Traverse up the DOM tree until reaching the root element
+while (element) {
+    level++;
+    element = element.parentElement;
+}
+alert("The level of the element is: " + level);
